26b904f1e07aeb1d9bbf14eff79cecc8a055aa89
ccc118 <91686791+ccc118@users.noreply.github.com>
2023-04-08 22:10:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 
 
-option = st.selectbox('Live today', ('Manchester United vs Brighton', 'Chelsea vs Brentford))
+option = st.selectbox('Live today', ('Manchester United vs Brighton', 'Chelsea vs Brentford'))
 
 st.write('You selected:', option)
