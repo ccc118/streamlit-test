159255fb754fd23cc54f30c07115abb2555176de
CA <ccc_cc188@gmail.com>
2023-04-08 21:46:01 +0100

    test

---
+++ test.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+'# Avocado Prices dashboard'
+
+st.write('# Avocado Prices dashboard')
+st.write('# Avocado Prices dashboard')
+#st.title('Avocado Prices dashboard')
+st.markdown('''
+This is a dashboard showing the *average prices* of different types of :avocado:  
+Data source: [Kaggle](https://www.kaggle.com/datasets/timmate/avocado-prices-2020)
+''')
+st.header('Summary statistics')
+st.header('Line chart by geographies')
+
