cfc89b68b00c661ddebfb05a4aa50c5af62f03ae
ccc118 <91686791+ccc118@users.noreply.github.com>
2023-04-08 22:03:25 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,9 @@ import pandas as pd
 '# Avocado Prices dashboard'
 
 st.write('# Avocado Prices dashboard')
-st.write('# Avocado Prices dashboard')
-#st.title('Avocado Prices dashboard')
 st.markdown('''
 This is a dashboard showing the *average prices* of different types of :avocado:  
 Data source: [Kaggle](https://www.kaggle.com/datasets/timmate/avocado-prices-2020)
 ''')
 st.header('Summary statistics')
-st.header('Line chart by geographies')
 
